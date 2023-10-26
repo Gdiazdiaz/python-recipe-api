e179ecaf1f4655a48133237fc51f1d8ad8c54019
Glenda Diaz <glendadiazah@outlook.com>
2023-10-26 13:37:25 -0600

    Adding core and testing

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,11 @@
+"""
+wait for DB
+""" 
+
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    """Command to wait for DB"""
+
+    def handle(self, *args, **options):
+        pass
