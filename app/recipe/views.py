11ad6d75bc994e22b33bcb78d67f239ca4a3eb3a
Glenda Diaz <glendadiazah@outlook.com>
2023-11-08 16:33:25 -0600

    Recipe details and tags

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -7,11 +7,23 @@ from recipe import serializers
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """Manage recipes in the database"""
-    serializer_class = serializers.RecipeSerializer
+    serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
-    authentication_classes = (TokenAuthentication)
-    permission_classes = (IsAuthenticated,)
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         """Retrieve recipes for authenticated users"""
         return self.queryset.filter(user=self.request.user).order_by('-id')
+    
+    def get_serializer_class(self):
+        """Return appropriate serializer class"""
+        if self.action =='list':
+            return serializers.RecipeSerializer
+        
+        return self.serializer_class
+    
+    def perform_create(self, serializer):
+        """Create a new recipe"""
+        serializer.save(user=self.request.user)
+        
